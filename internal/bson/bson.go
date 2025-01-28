dc8aa805acf7ed19739c47755257ebf144e2a808
Faison Zutavern <faisonz@protonmail.com>
2025-01-28 16:15:45 -0600

    Part 1 of bson

---
+++ internal/bson/bson.go
@@ -0,0 +1,99 @@
+package bson
+
+import "github.com/FaisonZ/bson/internal/bit"
+
+const (
+	BSON_VERSION  = 0b0001
+	OBJECT_TOKEN  = 0b001
+	ARRAY_TOKEN   = 0b010
+	STRING_TOKEN  = 0b011
+	NUMBER_TOKEN  = 0b100
+	BOOLEAN_TOKEN = 0b101
+	NULL_TOKEN    = 0b110
+	FALSE         = 0b0
+	TRUE          = 0b1
+)
+
+func EncodeJson(a any, bb *bit.BitBuilder) error {
+	writeVersion(bb)
+	encodeValue(a, bb)
+	return nil
+}
+
+func writeVersion(bb *bit.BitBuilder) {
+	bb.AddBits(BSON_VERSION, 4)
+}
+
+func writeToken(tokenByte byte, bb *bit.BitBuilder) error {
+	bb.AddBits(tokenByte, 3)
+	return nil
+}
+
+func writeTokenWithLength(
+	tokenByte byte,
+	lengthByte byte,
+	bb *bit.BitBuilder,
+) error {
+	writeToken(tokenByte, bb)
+	bb.AddBits(lengthByte, 5)
+	return nil
+}
+
+func writeString(s string, bb *bit.BitBuilder) error {
+	bb.AddBytes([]byte(s))
+
+	return nil
+}
+
+func encodeValue(a any, bb *bit.BitBuilder) error {
+	if o, ok := a.(map[string]any); ok {
+		encodeObject(o, bb)
+	} else if s, ok := a.(string); ok {
+		encodeString(s, bb)
+	} else if ar, ok := a.([]any); ok {
+		encodeArray(ar, bb)
+	} else if b, ok := a.(bool); ok {
+		encodeBoolean(b, bb)
+	}
+
+	return nil
+}
+
+func encodeString(s string, bb *bit.BitBuilder) error {
+	writeTokenWithLength(STRING_TOKEN, uint8(len(s)), bb)
+	writeString(s, bb)
+
+	return nil
+}
+
+func encodeBoolean(b bool, bb *bit.BitBuilder) error {
+	writeToken(BOOLEAN_TOKEN, bb)
+
+	if b {
+		bb.AddBits(TRUE, 1)
+	} else {
+		bb.AddBits(FALSE, 1)
+	}
+	return nil
+}
+
+func encodeArray(s []any, bb *bit.BitBuilder) error {
+	writeTokenWithLength(ARRAY_TOKEN, uint8(len(s)), bb)
+
+	for _, a := range s {
+		encodeValue(a, bb)
+	}
+
+	return nil
+}
+
+func encodeObject(o map[string]any, bb *bit.BitBuilder) error {
+	writeTokenWithLength(OBJECT_TOKEN, uint8(len(o)), bb)
+
+	for key, a := range o {
+		encodeString(key, bb)
+		encodeValue(a, bb)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/FaisonZ/bson/internal/bit"
+	"github.com/FaisonZ/bson/internal/bson"
 )
 
-func main() {
-	fmt.Println("Hello world")
+func runBitBuilder() {
 	b := bit.NewBitBuilder()
 	b.AddBits(0b0001, 4)
 	b.AddBits(0b001, 3)
@@ -18,4 +19,30 @@ func main() {
 	b.AddBits(0b011, 3)
 	b.AddBits(0b00011, 5)
 	b.AddBytes([]byte("bar"))
+	fmt.Println(b)
+}
+
+func main() {
+	var jsonBlob = []byte(`[{
+    "foo": "bar"
+},
+"foo",
+true,
+false
+]`)
+
+	var data any
+	bb := bit.NewBitBuilder()
+
+	err := json.Unmarshal(jsonBlob, &data)
+
+	if err != nil {
+		fmt.Printf("Unmarshal error: %q", err)
+		return
+	}
+
+	bson.EncodeJson(data, bb)
+	fmt.Println(bb)
+
+	runBitBuilder()
 }
